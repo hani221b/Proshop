768b8817c3148bc51f1c1e046316cf687edd17ac
hani221b <haniabdulfattah32@gmail.com>
2024-06-28 23:21:48 +0400

    finishing the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1>Welcome to proshop</h1>
+    <>
+    <Header />
+    <main className='py-3'>
+      <Container>
+        <Outlet />
+      </Container>
+    </main>
+    <Footer />
+    </>
   )
 }
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom"
+import "./assets/styles/index.css";
+import "./assets/styles/bootstrap.custom.css";
+import HomeScreen from './Screens/HomeScreen';
+import ProductScreen from './Screens/ProductScreen';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+        <Route path='/' index={true} element={<HomeScreen />} />
+        <Route path='/product/:id'  element={<ProductScreen />} />
+    </Route>
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
