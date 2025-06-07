576517dfd86e1fdef83f7c9b83fcf6695e719216
Hani Abdulfattah <haniabdulfattah32@gmail.com>
2025-06-07 11:37:05 +0400

    enabled docker-compose for local development

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = "";
-export const PRODUCTS_URL = "http://proshop.local/api/products";
-export const USERS_URL = "http://proshop.local/api/users";
-export const ORDERS_URL = "http://proshop.local/api/orders";
+export const PRODUCTS_URL = process.env.REACT_APP_PRODUCTS_URL;
+export const USERS_URL = process.env.REACT_APP_USERS_URL;
+export const ORDERS_URL = process.env.REACT_APP_ORDERS_URL;
 export const PAYPAL_URL = "/api/config/paypal";
